56ea3ccbe4ad06a9a0515b9f5f43944c005d65f7
Amit Ron <ish@ish.works>
2022-11-22 17:33:40 +0200

    Initial Commit

---
+++ simpleaac/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  simpleaac
+//
+//  Created by Amit Ron on 22/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simpleaac/simpleaacApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleaacApp.swift
+//  simpleaac
+//
+//  Created by Amit Ron on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct simpleaacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
